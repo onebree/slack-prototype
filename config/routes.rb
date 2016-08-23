7bbe2b6adea1da928619f3c590111a8b6a095b0b
Hunter Stevens <onebree@gmail.com>
2016-08-23 15:18:44 -0400

    Move mockup to different controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  root "messages#index"
+  root "home#mockup"
   
-  resources :messages, only: [:index]
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

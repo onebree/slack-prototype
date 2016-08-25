217ec74d4e70b7ec31cf58c0e6f78301567b43c8
Hunter Stevens <onebree@gmail.com>
2016-08-25 11:56:50 -0400

    Minimal implementation of user conversations (DMs)

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -5,4 +5,15 @@ class ConversationsController < ApplicationController
     @conversation = Conversation.find(params[:id])
     @grouped_messages = @conversation.messages.order(:created_at => :asc).group_by { |m| m.created_at.strftime("%A, %B %-d") }
   end
+
+  def create
+    @conversation = Conversation.new(:participant_ids => [1, params[:participant_id]])
+    if @conversation.save
+      redirect_to @conversation
+    else
+      raise
+      # Render last stored location with modal shown
+      # Show error message -- must have one participant
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
 
   resources :channels, only: [:show]
   resources :messages, only: [:create]
-  resources :conversations, only: [:show]
+  resources :conversations, only: [:show, :create]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

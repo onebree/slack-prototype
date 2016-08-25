382005d5c385f64cc2f7403c5293ac712674636a
Hunter Stevens <onebree@gmail.com>
2016-08-25 16:08:26 -0400

    Add user auth and signup page

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,10 @@ class User < ApplicationRecord
   has_many :channels
   has_many :messages, :foreign_key => "sender_id"
 
+  has_secure_password
+
+  validates :password, :presence => true, :length => { :minimum => 8 }
+
   def conversations
     Conversation.where("participant_one_id = :id OR participant_two_id = :id", :id => self.id).order(:id)
   end
--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,8 @@ Rails.application.routes.draw do
   resources :messages, only: [:create]
   resources :conversations, only: [:show, :create]
 
+  get "/signup", :to => "users#new"
+  resources :users, :only => [:new, :create]
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

9e0eaceb06ff6ebefa585f8a0fa00dafe0e429cf
Hunter Stevens <onebree@gmail.com>
2016-08-25 14:35:29 -0400

    Forgot self

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   has_many :channels
   has_many :messages, :foreign_key => "sender_id"
 
-  def conversations
+  def self.conversations
     Conversation.where("participant_one_id = :id OR participant_two_id = :id", :id => self.id)
   end
 end

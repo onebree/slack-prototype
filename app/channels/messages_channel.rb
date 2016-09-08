c9e37f9dbacdfb6f512693b6572725225cad8cce
Hunter Stevens <onebree@gmail.com>
2016-09-08 15:10:49 -0400

    Define subscribed and send_message methods for channel

--- app/channels/messages_channel.rb
+++ app/channels/messages_channel.rb
@@ -1,7 +1,9 @@
 # Be sure to restart your server when you modify this file. Action Cable runs in a loop that does not support auto reloading.
 class MessagesChannel < ApplicationCable::Channel
   def subscribed
-    # stream_from "some_channel"
+    receiveable_type = params["receivable_type"]
+    receivable_id = params["receivable_id"]
+    stream_from "messages_#{receivable_type}_#{receviable_id}_channel"
   end
 
   def unsubscribed
@@ -9,5 +11,10 @@ class MessagesChannel < ApplicationCable::Channel
   end
 
   def send_message(data)
+    current_user.messages.create!(
+      :body            => data["message"],
+      :receivable_type => data["receivable_type"],
+      :receivable_id   => data["receivable_id"]
+    )
   end
 end

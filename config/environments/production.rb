198f441db8362d92c247d3b9f3baa1c2cf6f435d
Hunter Stevens <onebree@gmail.com>
2016-09-12 12:24:46 -0400

    Add settings for action cable on heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -36,8 +36,8 @@ Rails.application.configure do
 
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
-  # config.action_cable.url = 'wss://example.com/cable'
-  # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
+  config.action_cable.url = "wss://shielded-plains-61557.herokuapp.com/cable"
+  config.action_cable.allowed_request_origins = ["http://shielded-plains-61557.herokuapp.com", "http://shielded-plains-61557.herokuapp.com"]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true

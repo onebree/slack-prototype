f6519aa2fab88c76bb2ee5e4bf1ae2c6ca3ccd73
Hunter Stevens <onebree@gmail.com>
2016-09-20 16:20:08 -0400

    Try conditional statement in AttachmentUploader

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -14,7 +14,5 @@ CarrierWave.configure do |config|
     config.fog_directory  = ENV["S3_BUCKET_NAME"]
     config.fog_public     = true
     config.fog_attributes = { "Cache-Control" => "max-age=#{365.day.to_i}" }
-  else
-    config.storage = :file
   end
 end

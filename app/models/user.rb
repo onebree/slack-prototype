2194f097d881a1c78dab21c9e58ca4b5822fffca
Hunter Stevens <onebree@gmail.com>
2016-10-10 16:15:44 -0400

    Fix name regex

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,10 @@
 class User < ApplicationRecord
-  NAME_REGEX = /+w/
+  NAME_REGEX = /\w+/
 
   has_many :messages
 
   validates :username, :presence => true, :uniqueness => { :case_sensitive => false },
-                       :format => { :with => /\A#{NAME_ERGEX}\z/i },
+                       :format => { :with => /\A#{NAME_REGEX}\z/i },
                        :length => { :maximum => 15 }
 
   validates :password, :presence => true, :length => { :minimum => 8 }
